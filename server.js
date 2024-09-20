990dc1adf7b1e97b6e7d8995b9c3bf172a0da145
Soko77788 <NickSokolowski7@gmail.com>
2024-09-20 09:31:58 -0500

    post and delete route added

--- server.js
+++ server.js
@@ -19,6 +19,51 @@ app.get('/api/movies', async (req, res) => {
     }
 })
 
+app.get('/api/movie-reviews', async (req, res) => {
+    try {
+    const result = await pool.query(`
+        SELECT * FROM movies 
+        JOIN reviews
+        ON movies.id = reviews.movie_id;`)
+    res.json(result.rows)
+    } catch (err) {
+        res.status(500).send('Error fetching movies')
+    }
+})
+
+app.post('/api/add-movie', async (req, res) => {
+    console.log(req.body)
+    const { movie_name } = req.body
+
+    if (!movie_name) {
+        return res.status(400).send('We need a movie name')
+    }
+
+    try {
+    const result = await pool.query(`
+        INSERT INTO movies (movie_name)
+        VALUES ($1);
+        `, [movie_name])
+    res.json(result.rowCount)
+    } catch (err) {
+        res.status(500).send('Error adding movie')
+    }
+})
+
+
+app.delete('/api/movie/:id', async (req, res) => {
+
+    const { id } = req.params
+
+
+    try {
+    const result = await pool.query(`DELETE FROM movies WHERE id = $1;`, [id])
+     
+    res.json(result.rowCount)
+    } catch (err) {
+        res.status(500).send('Error deleting movie')
+    }
+})
 
 
 
